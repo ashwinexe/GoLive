cd58d0fd0dea745e4ddf72da98dff22097aee7a7
ashwinexe <53075480+ashwinexe@users.noreply.github.com>
2021-05-11 22:13:46 +0530

    Hamburger perfected

--- script.js
+++ script.js
@@ -1,45 +1,23 @@
 function goBack() {
     window.history.back();
 }
+function autoOpen() {
+    if (screen && screen.width > 480) {
+        document.getElementById("sidenav").style.width = "300px";
+        document.getElementById("main").style.marginLeft = "300px";
+        
+        //comment the two line below to show hamburger even on desktop
+        document.getElementById("hamburger").style.display ="none";
+        document.getElementById("closebtn").style.display ="none";
+    }
+  }
 
 function openNav() {
-    document.getElementById("sideNav").style.width = "300px";
-    document.getElementById("main").style.marginLeft="250px";
-}
-
-function closeNav() {
-    document.getElementById("sideNav").style.width = "0";
-    document.getElementById("main").style.marginLeft="0";
-}
-
-// jquery
-$(document).ready(function () {
-
-    $("#sidebar").mCustomScrollbar({
-         theme: "minimal"
-    });
-
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
-
-});
-
-//jquery 2
-$(document).ready(function () {
-
-    $("#sidebar").mCustomScrollbar({
-         theme: "minimal"
-    });
-
-    $('#sidebarCollapse').on('click', function () {
-        // open or close navbar
-        $('#sidebar').toggleClass('active');
-        // close dropdowns
-        $('.collapse.in').toggleClass('in');
-        // and also adjust aria-expanded attributes we use for the open/closed arrows
-        // in our CSS
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-    });
-
-});
+    document.getElementById("sidenav").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+  }
+  
+  function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
